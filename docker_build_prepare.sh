d41536134aac8cf8b89e404781eac462d92caa80
r2h2 <rainer@hoerbe.at>
2016-02-04 21:28:11 +0100

    initial

--- docker_build_prepare.sh
+++ docker_build_prepare.sh
@@ -2,21 +2,6 @@
 # Prepare the build environment for docker build
 
 
-## unused !!!!!
-
-
-dir=$(dirname `which $0`)  #absolute dirname of script
-cd ${dir}
-
-repodir='PVZD'
-repourl='https://github.com/rhoerbe/PVZD.git'
-get_repo
-
-cd $repodir/dependent_pkg
-repodir='ordereddict-1.1'   # TODO: use wildcard to remove version
-repourl='https://pypi.python.org/pypi/ordereddict'
-get_repo
-
 get_repo() {
     # get PVZD from master (release branch)
     if [-e $repodir ] then
@@ -25,3 +10,20 @@ get_repo() {
         git clone $repourl # first time
     fi
 }
+
+dir=$(dirname `which $0`)  #absolute dirname of script
+cd ${dir}/opt
+
+repodir='PVZDjava'
+repourl='https://github.com/rhoerbe/${repodir}.git'
+get_repo
+
+repodir='PVZDpolman'
+repourl='https://github.com/rhoerbe/${repodir}.git'
+#get_repo
+
+cd $repodir/dependent_pkg
+repodir='ordereddict-1.1'   # TODO: use wildcard to remove version
+repourl='https://pypi.python.org/pypi/ordereddict'
+#get_repo
+
