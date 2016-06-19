9f94c07b001fac12c8cd6c176c3a5c4e73f82719
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-19 15:47:07 +0200

    refactor mocca startup into extra script

---
+++ install/scripts/start_mocca.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# start MOCCA
+
+logger -p local0.info "MOCCA Webstart (lokale Bürgerkartenumgebung)"
+cp -pr --no-clobber /opt/setup/mocca_settings/.java/ /home/liveuser/
+cp -pr --no-clobber /opt/setup/mocca_settings/.mocca /home/liveuser/
+javaws http://webstart.buergerkarte.at/mocca/webstart/mocca.jnlp
+
+exit 0
