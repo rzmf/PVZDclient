7cced720fbbdc6dc758564a98ccc020dfeb8fcac
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-19 13:36:43 +0200

    run start.sh always with non-root privs

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -4,21 +4,29 @@
 # format debug output if using bash -x
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
+if [ $(id -u) -eq 0 ]; then
+    echo "do not run this script as root"
+    exit 1
+else
+    sudo="sudo"
+fi
+
 if [ $(id -u) -ne 0 ]; then
     logger -p local0.info  "Need to be root to start pcscd. No smartcard service available now."
     ./PAtool.sh --help
     bash
 fi
 logger -p local0.info "Starting Smartcard Service"
-pcscd
+
+$sudo /usr/sbin/pcscd
 
 logger -p local0.info "MOCCA Webstart (lokale Bürgerkartenumgebung)"
-dbus-launch gnome-terminal --zoom=0.65 --geometry=80x10 --hide-menubar -e "javaws http://webstart.buergerkarte.at/mocca/webstart/mocca.jnlp"
-sleep 5
+javaws http://webstart.buergerkarte.at/mocca/webstart/mocca.jnlp &
+sleep 7
 
 cd /opt/PVZDpolman/PolicyManager/bin
-sudo -u liveuser ./PAtool.sh --help
+./PAtool.sh --help
 logger -p local0.info "Starting PAtoolGui"
-sudo -u liveuser ./PAtoolGui.sh
+./PAtoolGui.sh
 echo "bash: exit to terminate container; scripts in local directory to start pvzd tools."
-sudo -u liveuser bash
+bash
