8e8ad0ac65dfdd10c7af977141e9571326fffe8e
r2h2 <rainer@hoerbe.at>
2016-10-14 18:50:00 +0200

    new

---
+++ install/scripts/start_cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# startup script used in entrypoint of the docker container
+
+# format debug output if using bash -x
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+if [ $(id -u) -eq 0 ]; then
+    echo "do not run this script as root"
+    exit 1
+else
+    sudo="sudo"
+fi
+
+logger -p local0.info "Starting Smartcard Service"
+$sudo /usr/sbin/pcscd
+
+# start mocca and wait a bit for it to come up
+/start_mocca.sh &
+
+cd /opt/PVZDpolman/PolicyManager/bin
+
+/exec_PMP.sh
