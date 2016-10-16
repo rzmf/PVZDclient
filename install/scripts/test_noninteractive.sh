836de3a38c01d3316776ee7963110f45c959f3b9
r2h2 <rainer@hoerbe.at>
2016-10-16 23:35:54 +0200

    add testscript

---
+++ install/scripts/test_noninteractive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# run non-interactive unit tests
+
+if [ $(id -u) -eq 0 ]; then
+    echo "do not run this script as root"
+    exit 1
+fi
+
+cd /opt/PVZDpolman/PolicyManager/tests
+
+/testAll.sh -s noninteractive
